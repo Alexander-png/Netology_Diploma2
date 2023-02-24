eea476dd56a08986be5a10ae46ce43da630f17b4
Alexander <sanek41km@mail.ru>
2023-02-24 13:05:56 +0300

    Implemented retry command

--- Assets/Platformer/Scripts/UI/MenuSystem/Commands/Game/Retry.cs
+++ Assets/Platformer/Scripts/UI/MenuSystem/Commands/Game/Retry.cs
@@ -1,13 +1,11 @@
-using Platformer.EditorExtentions;
+using Platformer.GameCore;
 using Platformer.UI.MenuSystem.Commands.Base;
 
 namespace Platformer.UI.MenuSystem.Commands.Game
 {
 	public class Retry : MenuCommand
 	{
-        public override void Execute(object data)
-        {
-            GameLogger.AddMessage("TODO: retry");
-        }
+        public override void Execute(object data) =>
+            GameObserver.ReloadCurrentScene();
     }
 }
