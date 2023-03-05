dd9921ca4c5f3a8c59bdf5b2eb77bd47b51c9a78
Alexander <sanek41km@mail.ru>
2023-03-05 13:23:32 +0300

    Changed player health bar style

--- Assets/Platformer/Scripts/UI/HealthBar.cs
+++ Assets/Platformer/Scripts/UI/HealthBar.cs
@@ -1,7 +1,10 @@
 using Platformer.GameCore;
 using Platformer.PlayerSystem;
+using Platformer.UI.Helpers;
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace Platformer.UI
@@ -10,30 +13,40 @@ namespace Platformer.UI
 	{
 		[Inject]
 		private GameSystem _gameSystem;
-		[SerializeField]
+
 		private TMP_Text _text;
+        private float _currentHealth;
+        private Image _healthBarForeground;
 
-		private Player _player;
+        private Player _player;
 
-		private void Start() =>
+		private void Start()
+        {
             _gameSystem.GameLoaded += OnGameLoaded;
+            _healthBarForeground = GetComponentInChildren<InWorldHeathBarForeground>().GetComponent<Image>();
+            _text = GetComponentInChildren<TMP_Text>();
+        }
 
-        private void OnGameLoaded(object sender, System.EventArgs e)
+        private void OnGameLoaded(object sender, EventArgs e)
         {
             _gameSystem.GameLoaded -= OnGameLoaded;
             _player = _gameSystem.GetPlayer();
         }
 
-        private void LateUpdate()
-        {
+        private void LateUpdate() =>
             UpdateText();
-        }
 
         private void UpdateText()
         {
-            if (_player != null)
+            if (_player == null)
             {
-                _text.text = $"x:{_player.CurrentHealth}";
+                return;
+            }
+            if (_currentHealth != _player.CurrentHealth)
+            {
+                _currentHealth = _player.CurrentHealth;
+                _healthBarForeground.fillAmount = _currentHealth / _player.MaxHealth;
+                _text.text = $"{_healthBarForeground.fillAmount * 100}%";
             }
         }
     }
